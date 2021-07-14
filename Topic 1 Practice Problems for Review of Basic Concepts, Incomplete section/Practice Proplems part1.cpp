54b9623763b442a1d314c9d9e18ee0766142a531
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2021-07-14 22:07:42 +0200

    solve Q8

--- Topic 1 Practice Problems for Review of Basic Concepts, Incomplete section/Practice Proplems part1.cpp
+++ Topic 1 Practice Problems for Review of Basic Concepts, Incomplete section/Practice Proplems part1.cpp
@@ -8,3 +8,50 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }*/
+/*
+Q8
+0 points possible (ungraded)
+Consider the following C++ program
+
+
+#include <iostream>
+using namespace std;
+int main ( ) {
+    int n;
+    cout << "Give n: ";
+    cin >> n;
+    if (n < 0) {
+        switch (n) {
+            case 0:   cout <<"so ";
+            case -1:  cout << "how is your ";
+            case -10: cout << "parrot's ";
+                break;
+            default:  cout << "cat's ";
+        }
+        cout << "health?";
+    }
+    else {
+        switch (n) {
+            case 0: cout <<"so ";
+            case 1: cout << "how is your ";
+            case 10: cout << "cat's ";
+            default: cout << "health?";
+        }
+    }
+    cout << endl;
+    return 0;
+}
+//Which of the following strings CANNOT be printed by the above program for any value of the input 'n'?
+//solution: so how is your health?
+Explanation
+
+Option A is printed if the number n is -1
+
+Option B is printed if the number 'n' is 0. Note that in this case, the condition in the 'if' statement is not satisfied (since 0 is not < 0), so the program enters the 'else' block.
+
+Option C cannot be printed because in both the 'if' and 'else' blocks, there is no 'break' statement after printing "how is your ". Therefore, after printing "how is your " and before printing "health?" either "parrot's " or "cat's " must be printed (fall-through in 'switch-case' statement without 'break'). Consequently, the string "so how is your health?" cannot be printed.
+
+Option D can be printed if the number 'n' is -10. This is easy to see.
+
+Since C is the correct answer E cannot be the correct answer
+ */
