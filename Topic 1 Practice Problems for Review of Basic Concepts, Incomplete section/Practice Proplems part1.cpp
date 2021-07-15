822ad73026799bc52638e9c0b28fcf9e1568a9b5
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2021-07-15 21:44:08 +0200

    solve Q10

--- Topic 1 Practice Problems for Review of Basic Concepts, Incomplete section/Practice Proplems part1.cpp
+++ Topic 1 Practice Problems for Review of Basic Concepts, Incomplete section/Practice Proplems part1.cpp
@@ -55,3 +55,24 @@ Option D can be printed if the number 'n' is -10. This is easy to see.
 
 Since C is the correct answer E cannot be the correct answer
  */
+//Q10
+#include<iostream>
+using namespace std;
+int main(){
+   int A[]={0,1,2,3,4,5,6,7};
+   int n=8;
+   int i,j,k,l,temp;
+   int step=2;
+   for(i=0;i<n-step;i++){
+      for(j=i;j<i+step;j++){
+         temp=A[j];
+         A[j]=A[j+1];
+         A[j+1]=temp;
+      }
+      step=(step*2)-1;
+   }
+
+   for(i=0;i<n;i++)
+      cout<<A[i]<<" ";
+   return 0;
+}
