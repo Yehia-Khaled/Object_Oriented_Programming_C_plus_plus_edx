dc9ebc38d2221616e7f84f0f634dcbd3b353387d
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2021-07-24 00:25:08 +0200

    test C++ using Clion

---
+++ Test.cpp
@@ -0,0 +1,10 @@
+﻿//
+// Created by YEHIA on 7/24/2021.
+//
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Test!";
+    return 0;
+}
+
---
+++ main1.cpp
@@ -0,0 +1,10 @@
+//
+// Created by YEHIA on 7/24/2021.
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"main world!";
+    return 0;
+}
+
+
